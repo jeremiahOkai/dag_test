245ea8ae574d40ea1196780661182a9dd0fe60dc
Jeremiah Okai <jeremiah.okai@mendix.com>
2022-02-04 12:11:53 +0100

    correct errors

--- new_git_pull.py
+++ new_git_pull.py
@@ -1,14 +1,21 @@
 from datetime import datetime
+import datetime as dt
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
+default_args = {
+    "owner": "dwh",
+    "start_date": datetime(2018, 8, 16),
+    "retries": 1,
+    "retry_delay": dt.timedelta(minutes=5),
+}
 
 with DAG(
-    "git_pull_dags",
-    start_date=datetime(2022, 1, 1),
-    schedule_interval="@daily",
-    catchup=False,
+    "git_pull_dags_new",
+    default_args=default_args
+    schedule_interval="*/30 * * * *",
+    catchup=False
 ) as dag:
     git_pull = BashOperator(
         task_id="git_pull", bash_command="source /opt/airflow/gitpull.sh"
