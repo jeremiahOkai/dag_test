8f20ffe1310f705397eab38e397679dac32381b7
Jeremiah Okai <jeremiah.okai@mendix.com>
2022-02-07 17:53:26 +0100

    dag test

---
+++ notComposeGitPull.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+import datetime as dt
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "dwh",
+    "start_date": datetime(2018, 8, 16),
+    "retries": 1,
+    "retry_delay": dt.timedelta(minutes=5),
+}
+
+with DAG(
+    "new_git_pull_dags",
+    default_args=default_args,
+    description="A simple tutorial DAG",
+    schedule_interval="*/30 * * * *",
+    catchup=False,
+    tags=["example"],
+) as dag:
+    git_pull = BashOperator(
+        task_id="git_pull", bash_command="/usr/local/airflow/gitpull.sh "
+    )
+
+    git_pull
